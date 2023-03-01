01233d33823efb22098b827818a567c59d56f702
Nikolai Efremov <efremovnikolaus@gmail.com>
2023-03-01 18:41:54 +0100

    second, methods stackTrace

---
+++ src/main/java/Java Syntax Zero/Lesson5.8/SolutionTwo.java
@@ -0,0 +1,25 @@
+public class SolutionTwo {
+    public static final String OUTPUT_FORMAT = "Метод %s вызван из строки %d класса %s в файле %s.\n";
+
+    public static void main(String[] args) {
+        makeScrewdriver();
+    }
+
+    public static void printStackTrace(StackTraceElement[] stackTrace) {
+        for (StackTraceElement element : stackTrace) {
+            System.out.printf(OUTPUT_FORMAT, element.getMethodName(), element.getLineNumber(), element.getClassName(), element.getFileName());
+        }
+    }
+
+    static void makeScrewdriver() {
+        addJuice();
+    }
+
+    static void addJuice() {
+        addVodka();
+    }
+
+    static void addVodka() {
+        printStackTrace(Thread.currentThread().getStackTrace());
+    }
+}
