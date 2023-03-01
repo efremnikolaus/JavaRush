5fa5fc48156dae310c0fdae9406cdb14d5238a54
Nikolai Efremov <efremovnikolaus@gmail.com>
2023-03-01 18:57:24 +0100

    third, Stack methods

---
+++ src/main/java/Java Syntax Zero/Lesson5.8/SolutionThree.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class SolutionThree {
+
+    private final List<String> storage = new LinkedList<>();
+
+    public void push(String s) {
+        storage.add(0, s);
+    }
+
+    public String pop() {
+        return storage.remove(0);
+    }
+
+    public String peek() {
+        return storage.get(0);
+    }
+
+    public boolean empty() {
+        return storage.isEmpty();
+    }
+
+    public int search(String s) {
+        return storage.indexOf(s);
+    }
+}
