0d46e6381c8e37768022d279bac250bb19399eee
Nikolai Efremov <efremovnikolaus@gmail.com>
2023-03-04 22:03:04 +0100

    renamed files

---
+++ java/src/main/java/org/example/Java Syntax Zero/Lesson 5.8/FiveSolutionFourEight.java
@@ -0,0 +1,14 @@
+public class FiveSolutionFourEight {
+
+    public static void main(String[] args) {
+        try {
+            dangerousMethod();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void dangerousMethod() throws Exception {
+        throw new Exception("Mu-ha-ha!");
+    }
+}
