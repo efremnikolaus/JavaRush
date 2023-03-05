1444d4b1b1033114b7b6545287c8b5ca187d8eaa
Nikolai Efremov <efremovnikolaus@gmail.com>
2023-03-05 13:19:09 +0100

    test commit

---
+++ java/src/main/java/org/exampl3/JavaSyntaxZer0/Lesson6_3/SixSolutionTwoThree.java
@@ -0,0 +1,10 @@
+package org.exampl3.JavaSyntaxZer0.Lesson6_3;
+import java.util.Scanner;
+
+public class SixSolutionTwoThree {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        System.out.println(string);
+    }
+}
