b34bd9d51caaa29798bcc9daf273e8f92d3b8e7a
Nikolai Efremov <efremovnikolaus@gmail.com>
2023-03-08 22:31:58 +0100

    interesting use

--- java/src/main/java/org/exampl3/JavaSyntaxZer0/Lesson6_4/SixSolutionFourFour.java
+++ java/src/main/java/org/exampl3/JavaSyntaxZer0/Lesson6_4/SixSolutionFourFour.java
@@ -1,10 +1,17 @@
 package org.exampl3.JavaSyntaxZer0.Lesson6_4;
 
+import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 public class SixSolutionFourFour {
     public static void main(String[] args) {
-        PrintStream stream = System.out;
-        stream.println("Hello world!");
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try (PrintStream stream = new PrintStream(baos)) {
+            stream.println("Hello");
+            stream.println(123);
+        }
+
+        String result = baos.toString();
+        System.out.println(result);
     }
 }
