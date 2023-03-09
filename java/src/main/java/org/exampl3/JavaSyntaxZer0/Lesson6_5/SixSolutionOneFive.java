7e389434ae5f9ee8b6c93afee2f1cf801048cd6c
Nikolai Efremov <efremovnikolaus@gmail.com>
2023-03-09 12:22:35 +0100

    test File Path

---
+++ java/src/main/java/org/exampl3/JavaSyntaxZer0/Lesson6_5/SixSolutionOneFive.java
@@ -0,0 +1,10 @@
+package org.exampl3.JavaSyntaxZer0.Lesson6_5;
+
+import java.io.File;
+
+public class SixSolutionOneFive {
+    public static void main(String[] args) {
+        File file = new File("C:\\file.txt");
+        System.out.println(file.getPath());
+    }
+}
