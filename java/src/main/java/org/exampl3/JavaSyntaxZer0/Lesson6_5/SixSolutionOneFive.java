ca335f161a396602248ca4c90c3802e82957a2e9
Nikolai Efremov <efremovnikolaus@gmail.com>
2023-03-09 12:37:23 +0100

    list files

--- java/src/main/java/org/exampl3/JavaSyntaxZer0/Lesson6_5/SixSolutionOneFive.java
+++ java/src/main/java/org/exampl3/JavaSyntaxZer0/Lesson6_5/SixSolutionOneFive.java
@@ -4,6 +4,7 @@ import java.io.File;
 
 public class SixSolutionOneFive {
     public static void main(String[] args) {
-        System.out.println(new File("C:\\file.txt").isDirectory());
+        File file = new File("C:\\file.txt");
+        File[] filesInCDrive = file.listFiles();
     }
 }
