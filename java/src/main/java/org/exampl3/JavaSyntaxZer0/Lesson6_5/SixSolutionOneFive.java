d51606445e7cb7a901d471a8bf17cad0fd81f9c6
Nikolai Efremov <efremovnikolaus@gmail.com>
2023-03-09 12:39:03 +0100

    parent file

--- java/src/main/java/org/exampl3/JavaSyntaxZer0/Lesson6_5/SixSolutionOneFive.java
+++ java/src/main/java/org/exampl3/JavaSyntaxZer0/Lesson6_5/SixSolutionOneFive.java
@@ -5,6 +5,6 @@ import java.io.File;
 public class SixSolutionOneFive {
     public static void main(String[] args) {
         File file = new File("C:\\file.txt");
-        File[] filesInCDrive = file.listFiles();
+        File folder = file.getParentFile();
     }
 }
