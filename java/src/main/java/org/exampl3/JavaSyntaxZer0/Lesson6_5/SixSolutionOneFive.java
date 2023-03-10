f0b46103fab9312c993dcc212dd9590dffffa5c6
Nikolai Efremov <efremovnikolaus@gmail.com>
2023-03-10 23:48:48 +0100

    get Root (System.in)

--- java/src/main/java/org/exampl3/JavaSyntaxZer0/Lesson6_5/SixSolutionOneFive.java
+++ java/src/main/java/org/exampl3/JavaSyntaxZer0/Lesson6_5/SixSolutionOneFive.java
@@ -1,10 +1,13 @@
 package org.exampl3.JavaSyntaxZer0.Lesson6_5;
 
-import java.io.File;
+import java.util.Scanner;
+import java.nio.file.Path;
+
 
 public class SixSolutionOneFive {
-    public static void main(String[] args) {
-        File file = new File("C:\\file.txt");
-        file.mkdirs();
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        Path diskName = Path.of(line).getRoot();
     }
 }
