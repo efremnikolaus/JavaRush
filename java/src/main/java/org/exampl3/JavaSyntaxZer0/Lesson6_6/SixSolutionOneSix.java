b67a9b7215a22eb0fc7ce73311b6b36a86947146
Nikolai Efremov <efremovnikolaus@gmail.com>
2023-03-11 14:25:34 +0100

    test methods of Files import

---
+++ java/src/main/java/org/exampl3/JavaSyntaxZer0/Lesson6_6/SixSolutionOneSix.java
@@ -0,0 +1,12 @@
+package org.exampl3.JavaSyntaxZer0.Lesson6_6;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class SixSolutionOneSix {
+    public static void main(String[] args) {
+        Files.createFile(Path.of("asddas"));
+        Files.createDirectory(Path.of("sdfs"));
+        Files.createDirectories(Path.of("asda"));
+    }
+}
