407c37ea279122534db1b5b1b09fb0e808b1a180
Nikolai Efremov <efremovnikolaus@gmail.com>
2023-03-11 14:35:53 +0100

    Work with File

--- java/src/main/java/org/exampl3/JavaSyntaxZer0/Lesson6_6/SixSolutionOneSix.java
+++ java/src/main/java/org/exampl3/JavaSyntaxZer0/Lesson6_6/SixSolutionOneSix.java
@@ -1,12 +1,17 @@
 package org.exampl3.JavaSyntaxZer0.Lesson6_6;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Scanner;
+import java.util.List;
 
 public class SixSolutionOneSix {
-    public static void main(String[] args) {
-        Files.createFile(Path.of("asddas"));
-        Files.createDirectory(Path.of("sdfs"));
-        Files.createDirectories(Path.of("asda"));
+    public static void main(String[] args) throws IOException {
+        Path path = Path.of("c:\\readme.txt");
+        List<String> list = Files.readAllLines(path);
+        for(String str : list) {
+            System.out.println(str);
+        }
     }
 }
