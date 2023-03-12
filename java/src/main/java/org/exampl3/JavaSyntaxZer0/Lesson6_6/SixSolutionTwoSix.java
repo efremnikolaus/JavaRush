731fa43b59ed7ba43e671e61ee2f7fa6096f691d
Nikolai Efremov <efremovnikolaus@gmail.com>
2023-03-12 22:25:37 +0100

    Directory Stream

--- java/src/main/java/org/exampl3/JavaSyntaxZer0/Lesson6_6/SixSolutionTwoSix.java
+++ java/src/main/java/org/exampl3/JavaSyntaxZer0/Lesson6_6/SixSolutionTwoSix.java
@@ -1,19 +1,25 @@
 package org.exampl3.JavaSyntaxZer0.Lesson6_6;
 
-import java.io.IOException;
 import java.nio.file.DirectoryStream;
-import java.nio.file.Path;
 import java.nio.file.Files;
+import java.util.Scanner;
+import java.nio.file.Path;
 
 public class SixSolutionTwoSix {
+    private static final String THIS_IS_FILE = " - это файл";
+    private static final String THIS_IS_DIR = " - это директория";
+
     public static void main(String[] args) {
-        Path path = Path.of("c:\\windows");
-        try(DirectoryStream<Path> files = Files.newDirectoryStream(path)){
-            for(Path path2 : files){
-                System.out.println(path2);
+        Scanner scanner = new Scanner(System.in);
+        Path directory = Path.of(scanner.nextLine());
+        try (DirectoryStream<Path> paths = Files.newDirectoryStream(directory)) {
+            for (Path path : paths) {
+                if (Files.isRegularFile(path)) {
+                    System.out.println(path.toString() + THIS_IS_FILE);
+                } else if (Files.isDirectory(path)) {
+                    System.out.println(path.toString() + THIS_IS_DIR);
+                }
             }
-        } catch(IOException e){
-            System.out.println(e.getMessage());
         }
     }
 }
